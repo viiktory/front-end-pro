defbfe0a593f8bc87f14a782cc5537a9dc7f0fd4
viktoriia-tarandushka <t.viktoria.543@gmail.com>
2024-09-04 11:13:54 +0300

    add hw1

---
+++ lesson3_hw1/main.js
@@ -0,0 +1,12 @@
+const name = "Viktoriia";
+let age = 26;
+let value = 10 > 3;
+let city;
+let email = null;
+
+
+console.log("type: " + typeof (name));
+console.log("type: " + typeof (age));
+console.log("type: " + typeof (value));
+console.log("type: " + typeof (city));
+console.log("type: " + typeof (email));
