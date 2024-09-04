46551fb97e5cc6621abe565388f17e8645cbbafa
viktoriia-tarandushka <t.viktoria.543@gmail.com>
2024-09-04 11:13:54 +0300

    add hw2

---
+++ lesson3_hw2/main.js
@@ -0,0 +1,5 @@
+let name = prompt('What is your name?');
+let age = prompt('What is your age?');
+let city = prompt('What is your city?');
+
+console.log(`Hi ${name}, your age is ${age}, your city is ${city}. Welcome to the team!`);
